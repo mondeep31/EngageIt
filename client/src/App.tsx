9fea185729a52b59e76b7b76eb3baf8dd57fadcf
Mondeep Chetry <mandeepchetry6969@gmail.com>
2025-02-19 03:29:19 +0530

    added sidebar

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import SidebarComp from "./components/Sidebar";
+
+const App = () => {
+  return (
+    <div className="bg-black">
+      <SidebarComp />
+    </div>
+  );
+};
+
+export default App;
